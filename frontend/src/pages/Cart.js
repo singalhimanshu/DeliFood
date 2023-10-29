52b41ffe3d422e87b961f6e943a0c1dbffe91ef6
Deepti <deepti22599@gmail.com>
2023-10-29 12:01:48 +0530

    update post api

--- frontend/src/pages/Cart.js
+++ frontend/src/pages/Cart.js
@@ -36,8 +36,11 @@ const Cart = (props) => {
     setIsCheckout(true)
   }
 
+  const localAPI = 'http://localhost:8080/api/orders'
+  const API = 'https://deli-food.vercel.app/api/orders'
+
   const submitOrderHandler = async (userData) => {
-    await fetch('http://localhost:8080/api/orders', {
+    await fetch(API, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
